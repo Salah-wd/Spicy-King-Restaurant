51d529d398d0c0d83b07a0e996586170226c2c07
Salah-wd <secondarychannel04@gmail.com>
2025-04-26 20:08:50 +0600

    Add files via upload

---
+++ rastoareant/script.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal(".logo", {
+  distance: "80px",
+  origin: "top",
+  delay: 300,
+  dration: 500,
+});
+
+ScrollReveal().reveal(".navbar-nav", {
+  distance: "80px",
+  origin: "left",
+  delay: 100,
+  duration: 1000,
+});
